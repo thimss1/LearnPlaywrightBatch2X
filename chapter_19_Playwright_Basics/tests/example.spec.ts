b11904ced0a527e35f4a524e9f49aca82f270f2b
thimss1 <ssmithga@gmail.com>
2026-06-20 21:42:48 -0500

    Update chapter_19_Playwright_Basics files

--- chapter_19_Playwright_Basics/tests/example.spec.ts
+++ chapter_19_Playwright_Basics/tests/example.spec.ts
@@ -1,18 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// page - Inbuilt fixture is automatically given to you,
+// which are the functions you can directly use in Playwright.
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test("Verify that the title  will be TTA Cart", async ({ page }) => {
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.goto("https://app.thetestingacademy.com/playwright/ttacart/")
+  await expect(page).toHaveTitle("TTACart - Login");
+  await page.waitForTimeout(5000);
 });
